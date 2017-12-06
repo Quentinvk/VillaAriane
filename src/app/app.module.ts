7d0f12018b6cc8a4f1a6ff7f7c6e40330bfb0166
Quentinvk <quentin.vankesteren.w4012@student.hogent.be>
2017-12-06 19:16:41 +0100

    resolved security issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
+import { CarouselComponent } from './carousel/carousel.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CalendarComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,

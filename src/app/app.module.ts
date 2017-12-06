6398f44db2832839b55481e16bfdadeb604ac851
Quentinvk <quentin.vankesteren.w4012@student.hogent.be>
2017-12-06 19:02:48 +0100

    added carousel carousel comp, modified home component, it's bugged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
+import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

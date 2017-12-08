1137834ac61e4e1f2a8c020ad787f18c757e7dd9
Quentinvk <quentin.vankesteren.w4012@student.hogent.be>
2017-12-08 16:00:02 +0100

    moddified bookingDataService

--- villaArianefrontend/src/app/booking-data.service.ts
+++ villaArianefrontend/src/app/booking-data.service.ts
@@ -5,8 +5,6 @@ import { Booking } from './booking/booking.model';
 export class BookingDataService {
 public _bookings = new Array<Booking>();
   constructor() {
-    let bookingTest = new Booking("Jean", "Luc", new Date(2017-12-09) ,new Date(2017-12-10),2,true);
-    this._bookings.push(bookingTest);
    }
    get bookings() : Booking[] {
       return this._bookings;

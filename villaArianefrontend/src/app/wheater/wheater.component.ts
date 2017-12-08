90a5209ef66d114c3c52f5be346902ada5a8038d
Quentinvk <quentin.vankesteren.w4012@student.hogent.be>
2017-12-08 13:20:57 +0100

    merge conflict

---
+++ villaArianefrontend/src/app/wheater/wheater.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wheater',
+  templateUrl: './wheater.component.html',
+  styleUrls: ['./wheater.component.css']
+})
+export class WheaterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
